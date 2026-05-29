9f95fa154f4a9921cdfc39a85c8d7b0bef8e0add
Lautaro Perez <lautaropepa123@gmail.com>
2026-05-29 10:59:19 -0300

    Basic structure

---
+++ src/main/java/com/utn/space/venueaapi/Controllers/Spaces_imagesController.java
@@ -0,0 +1,9 @@
+package com.utn.space.venueaapi.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/s&v")
+public class Spaces_imagesController {
+}
--- src/main/java/com/utn/space/venueaapi/Service/Space_imagesService.java
+++ src/main/java/com/utn/space/venueaapi/Service/Space_imagesService.java
@@ -1,4 +1,7 @@
 package com.utn.space.venueaapi.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Space_imagesService {
 }
