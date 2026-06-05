d798e5d89d0b5a7456936ee5b94253d01657e496
Federico Huergo <ffhuergo@gmail.com>
2026-06-05 11:02:44 -0300

    tratando

--- src/main/java/com/utn/space/venueaapi/service/ReservationService.java
+++ src/main/java/com/utn/space/venueaapi/service/ReservationService.java
@@ -118,6 +118,31 @@ public class ReservationService {
     }
 
 
+    public Reservation confirmReservation(Long id){
+        Reservation aux= reservationRepository.findById(id).orElseThrow(()->new ExceptionIdNotFound ("Reservation", id));
+        aux.setStatus(ReservationStatus.CONFIRMED);
+        return reservationRepository.save(aux);
+    }
+
+    public Reservation cancelReservation(Long id){
+        Reservation aux= reservationRepository.findById(id).orElseThrow(()->new ExceptionIdNotFound ("Reservation", id));
+        aux.setStatus(ReservationStatus.CANCELLED);
+        return reservationRepository.save(aux);
+    }
+
+    public Reservation completeReservation(Long id){
+        Reservation aux= reservationRepository.findById(id).orElseThrow(()->new ExceptionIdNotFound ("Reservation", id));
+        aux.setStatus(ReservationStatus.COMPLETED);
+        //falta sacarlo de googlecalendar
+        return reservationRepository.save(aux);
+    }
+
+    public Reservation softDelete(Long id){
+        Reservation aux= reservationRepository.findById(id).orElseThrow(()->new ExceptionIdNotFound ("Reservation", id));
+        aux.setIsActive(false);
+        return reservationRepository.save(aux);
+
+    }
   //  hola
 
 }
