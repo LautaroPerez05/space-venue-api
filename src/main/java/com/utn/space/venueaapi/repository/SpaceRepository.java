d38cdf4e6906fac35e8b37ea689a2f8268d2a1fc
lautaro <Lautaropepa123@gmail.com>
2026-06-05 10:11:16 -0300

    no se

--- src/main/java/com/utn/space/venueaapi/repository/SpaceRepository.java
+++ src/main/java/com/utn/space/venueaapi/repository/SpaceRepository.java
@@ -4,6 +4,8 @@ import com.utn.space.venueaapi.model.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SpaceRepository extends JpaRepository<Space,Long> {
 }
