723f4697a4dd8268cc27206264fd526f602a32e8
Federico Huergo <ffhuergo@gmail.com>
2026-06-12 17:50:32 -0300

    transforme ReservationDTO.java a record

--- src/main/java/com/utn/space/venueaapi/model/records/ReservationDTO.java
+++ src/main/java/com/utn/space/venueaapi/model/records/ReservationDTO.java
@@ -13,65 +13,59 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Component
 
 @Schema( description = "DTO de Reservas")
-public class ReservationDTO {
+public record ReservationDTO (
     @NotEmpty(groups = Update.class)
     @Schema(description = "Identificador único", example = "1")
-    private Integer id;
+    Integer id,
 
     @NotBlank(groups = {Create.class, Update.class})
     @Schema(description = "Nombre de la reserva", example = "Cumple de Sofia")
-    private String title;
+    String title,
 
     @NotBlank(groups = {Create.class, Update.class})
     @Schema(description = "Descripción del Evento", example = "El cumpleaños de 15 de Sofia")
-    private String description;
+    String description,
 
     @NotEmpty(groups = {Create.class, Update.class})
-    private String googleEventCode;
+    String googleEventCode,
 
     @NotEmpty(groups = {Create.class, Update.class})
     @Schema(description = "Fecha de Inicio del Evento", example = "2026-09-15T21:00:00")
-    private LocalDateTime fromDate;
+    LocalDateTime fromDate,
 
     @NotEmpty(groups = {Create.class, Update.class})
     @Schema(description = "Fecha de Fin del Evento", example = "2026-09-16T05:00:00")
-    private LocalDateTime untilDate;
+    LocalDateTime untilDate,
 
     @Schema(description = "Precio final de la reserva")
-    private Double finalPrice;
+    Double finalPrice,
 
     @NotEmpty(groups = {Create.class, Update.class})
     @Schema(description = "Estado de la Reserva", example = "CONFIRMED")
-    private ReservationStatus status;
+    ReservationStatus status,
 
     @NotEmpty(groups = {Create.class, Update.class})
     @Schema(description = "Fecha en la que se hizo la reserva", example = "2026-05-20T011:25:31")
-    private LocalDateTime createdAt;
+    LocalDateTime createdAt,
 
     @Schema(description = "Flag para saber si se hizo un softdelete en la reserva", example = "2026-05-20T011:25:31")
-    private Boolean isActive;
-
-    private Boolean saveToMyCalendar;
+    Boolean isActive,
+    Boolean saveToMyCalendar,
 
     @NotEmpty(groups = {Create.class, Update.class})
     @Schema(description = "ID del usuario que creo la reserva")
-    private Integer idConsumer;
+    Integer idConsumer,
 
     @NotEmpty(groups = {Create.class, Update.class})
     @Schema(description = "ID del espacio a reservar", example = "2026-05-20T011:25:31")
-    private Integer idSpace;
+    Integer idSpace,
 
 
     @Schema(description = "Lista de las IDs de los servicios agregados")
-    private List<Integer> idServicesSelec;
-
-
+    List<Integer> idServicesSelec
+){
     public Boolean getSaveToMyCalendar() {
         // Evita NullPointerException asignando falso por defecto si no es provisto en la petición JSON
         return saveToMyCalendar != null ? saveToMyCalendar : false;
