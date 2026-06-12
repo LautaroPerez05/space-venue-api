2b74879173de0e48c537b1b07fecb8c233a7bcb7
Lautaro <lautaropepa123@gmail.com>
2026-06-12 14:55:25 -0300

    baja de clientes

--- src/main/java/com/utn/space/venueaapi/controllers/ConsumerController.java
+++ src/main/java/com/utn/space/venueaapi/controllers/ConsumerController.java
@@ -14,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 import java.util.List;
 
 @AllArgsConstructor
