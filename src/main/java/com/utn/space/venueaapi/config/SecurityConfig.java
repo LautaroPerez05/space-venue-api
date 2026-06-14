3aa3594e5d9bc0929c55efd5693a885112094b2f
FeijooCiro4 <cirofeijoo2004@mail.com>
2026-06-13 22:38:37 -0300

    Adición del link del deploy del front para que CORS permita la conexion

--- src/main/java/com/utn/space/venueaapi/config/SecurityConfig.java
+++ src/main/java/com/utn/space/venueaapi/config/SecurityConfig.java
@@ -97,7 +97,7 @@ public class SecurityConfig {
                 "http://localhost:5500",      // Live Server de VS Code común
                 "http://127.0.0.1:5500",      // Live Server por IP
                 "http://127.0.0.1",           // Loopback local estándar
-                "https://tu-frontend-desplegado.onrender.com" // <- Reemplaza con tu link real del front
+                "https://space-venue-front.onrender.com/"
         ));
 
         // Métodos HTTP permitidos
