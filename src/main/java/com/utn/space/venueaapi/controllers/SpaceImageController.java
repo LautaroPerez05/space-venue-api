41872c605237322f5801f63bc769e67f3a95ae19
Federico Huergo <ffhuergo@gmail.com>
2026-06-15 14:43:09 -0300

    Termine el Swagger en SpaceImageController y SpaceImageDTO

--- src/main/java/com/utn/space/venueaapi/controllers/SpaceImageController.java
+++ src/main/java/com/utn/space/venueaapi/controllers/SpaceImageController.java
@@ -1,46 +1,113 @@
 package com.utn.space.venueaapi.controllers;
 
+import com.utn.space.venueaapi.model.flags.Create;
+import com.utn.space.venueaapi.model.flags.Update;
+import com.utn.space.venueaapi.model.records.ReservationDTO;
 import com.utn.space.venueaapi.model.records.SpaceImageDTO;
 import com.utn.space.venueaapi.model.SpaceImage;
 import com.utn.space.venueaapi.service.SpaceImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/spaceimages")
+@Tag(name = "Imágenes", description = "Operaciones sobre Imágenes de Espacios.")
+
 public class SpaceImageController {
     @Autowired
     SpaceImageService spaceImagesService;
 
     @GetMapping
+    @Operation(
+            summary = "Busca TODAS las Imágenes."
+    )
     public ResponseEntity<List<SpaceImage>> listSpaceImages(){
         return ResponseEntity.ok(spaceImagesService.findAll());
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Busca una Imagen por su ID."
+    )
     public ResponseEntity<SpaceImage> findSpaceImageById(@PathVariable Integer id){
         return ResponseEntity.ok(spaceImagesService.findById(id));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Borrar una Imagen."
+    )
     public void deleteSapceImageById(@PathVariable Integer id){
         spaceImagesService.deleteById(id);
     }
 
     @PostMapping()
-    public void insertSpace(@RequestBody SpaceImageDTO spaceImageDTO){
+    @Operation(
+            summary = "Crea una Imagen."
+    )
+    public void insertSpace(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Entra los datos obligatorios de la creación de una nueva Imagen",
+                    required = true,
+                    content = @Content(
+                            schema = @Schema(
+                                    implementation = SpaceImageDTO.class),
+                            examples = @ExampleObject(
+                                    name = "Ejemplo",
+                                    value = """
+                                    {
+                                      "idImage": null,
+                                      "idSpace":74,
+                                      "fileName":"Cocina.jpg",
+                                      "urlImage":,
+                                      "dateSend":"2026-09-15T21:00:00"}
+                                    """)
+                    )
+            )
+            @Validated(Create.class)@RequestBody SpaceImageDTO spaceImageDTO){
         spaceImagesService.insertSpaceImage(spaceImageDTO);
     }
 
     @PutMapping("/{id}")
-    public void modifySpaceImage(@PathVariable Integer id, @RequestBody SpaceImageDTO spaceImageDTO){
+    @Operation(
+            summary = "Modifica una Imagen."
+    )
+    public void modifySpaceImage(@PathVariable Integer id,
+                                 @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                         description = "Entra los datos obligatorios de la modificación de una Imagen",
+                                         required = true,
+                                         content = @Content(
+                                                 schema = @Schema(
+                                                         implementation = SpaceImageDTO.class),
+                                                 examples = @ExampleObject(
+                                                         name = "Ejemplo",
+                                                         value = """
+                                    {
+                                      "idImage": null,
+                                      "idSpace":74,
+                                      "fileName":"Cocina.jpg",
+                                      "urlImage":,
+                                      "dateSend":"2026-09-15T21:00:00"}
+                                    """)
+                                         )
+                                 )
+                                 @Validated(Update.class)@RequestBody SpaceImageDTO spaceImageDTO){
         spaceImagesService.modifySpaceImage(id,spaceImageDTO);
     }
 
     @GetMapping("/byspaceid/{id}")
+    @Operation(
+            summary = "Busca TODAS las Imágenes de un Espacio."
+    )
     public ResponseEntity<List<SpaceImage>> findAllBySpaceId(@PathVariable Integer id){
         return ResponseEntity.ok(spaceImagesService.findAllBySpaceId(id));
     }
--- src/main/java/com/utn/space/venueaapi/model/records/SpaceImageDTO.java
+++ src/main/java/com/utn/space/venueaapi/model/records/SpaceImageDTO.java
@@ -2,24 +2,32 @@ package com.utn.space.venueaapi.model.records;
 
 import com.utn.space.venueaapi.model.flags.Create;
 import com.utn.space.venueaapi.model.flags.Update;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
+@Schema( description = "DTO de Imagenes")
 public record SpaceImageDTO (
-        @NotEmpty(groups = {Create.class, Update.class})
+        @NotNull(groups = Update.class)
+        @Schema(description = "Identificador único", example = "1")
         Integer idImage,
 
         @NotEmpty(groups = {Create.class, Update.class})
+        @Schema(description = "Identificador del Espacio al que pertenece", example = "74")
         Integer idSpace,
 
         @NotBlank(groups = {Create.class, Update.class})
+        @Schema(description = "Nombre del achivo")
         String fileName,
 
         @NotBlank(groups = {Create.class, Update.class})
+        @Schema(description = "Dirección del archivo")
         String urlImage,
 
-        @NotEmpty(groups = {Create.class, Update.class})
+        @NotEmpty(groups =  Update.class)
+        @Schema(description = "Fecha en la que se subió la imagen",  example = "2026-06-13T011:25:31")
         LocalDateTime dateSend){
 }
